200c1fa09d658cb2159befd42377775cec39c6ea
makufff <Tanapatx.tyran@gmail.com>
2025-04-08 18:14:57 +0700

    fix : cell 02 - ex03

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
+n1 = int(input("Enter the first number:\n"))
+n2 = int(input("Enter the second number:\n"))
 
 ans = n1 * n2
 
 string = f"{n1} x {n2} = {ans} \n"
 
 if ans == 0:
-    print(string + "The ansult is positive and negative.")
+    print(string + "The result is positive and negative.")
 elif ans < 0:
-    print(string + "The ansult is negative.")
+    print(string + "The result is negative.")
 else:
-    print(string + "The ansult is positive.")
+    print(string + "The result is positive.")
