12a2f97d4b3ad93699ac4a05e492f9b798bf45c7
makufff <Tanapatx.tyran@gmail.com>
2025-04-08 20:03:57 +0700

    fix : cell 06

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def upcase_it(txt):
+    return txt.upper()
+
+print(upcase_it("hello"))
---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add_one(n):
+    return n + 1
+
+num = 5
+print("Before calling add_one:", num)
+
+add_one(num)
+
+print("After calling add_one:", num)
